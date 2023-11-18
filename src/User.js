8e872d65f662824ed674d6ea1af04d9ce8262ee3
Loui <143069855+LouiFuentes@users.noreply.github.com>
2023-11-18 18:10:37 +0800

    Add files via upload

---
+++ src/User.js
@@ -0,0 +1,87 @@
+export const User = [
+  {
+    id: 1,
+    name: {
+      firstname: "LOUI",
+      lastname: "FUENTES",
+    },
+    course: "BSIT",
+    yearsection: "3A",
+    username: "LOUI07",
+    password: "1234",
+    usertype: "STUDENT",
+  },
+  {
+    id: 2,
+    name: {
+      firstname: "ANGELO",
+      lastname: "MALDORA",
+    },
+    course: "BSIT",
+    yearsection: "3A",
+    username: "ANGELO",
+    password: "2345",
+    usertype: "STUDENT",
+  },
+  {
+    id: 3,
+    name: {
+      firstname: "JESSE",
+      lastname: "AMANG",
+    },
+    course: "BSIT",
+    yearsection: "3A",
+    username: "JESS",
+    password: "3456",
+    usertype: "STUDENT",
+  },
+  {
+    id: 4,
+    name: {
+      firstname: "JOHN",
+      lastname: "TABANYAG",
+    },
+    course: "BSIT",
+    yearsection: "3A",
+    username: "JONNYSINS",
+    password: "4567",
+    usertype: "STUDENT",
+  },
+  {
+    id: 5,
+    name: {
+      firstname: "JERRY",
+      lastname: "ALE",
+    },
+    course: "",
+    yearsection: "",
+    username: "JERRY",
+    password: "5678",
+    usertype: "TEACHER",
+  },
+  {
+    id: 6,
+    name: {
+      firstname: "GABRINE",
+      lastname: "DIAZ",
+    },
+    course: "",
+    yearsection: "",
+    username: "GABRINE",
+    password: "6789",
+    usertype: "TEACHER",
+  },
+  {
+    id: 7,
+    name: {
+      firstname: "FERMO",
+      lastname: "ASUFRA",
+    },
+    course: "",
+    yearsection: "",
+    username: "FERMO",
+    password: "7890",
+    usertype: "TEACHER",
+  },
+  
+];
